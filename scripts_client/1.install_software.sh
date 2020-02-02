7a567ac26d9b607ec042d0ba6b8cbae73f9b0f1f
masa3 <masa@hashicorp.com>
2020-02-02 12:27:09 +0900

    Script added

---
+++ scripts_client/1.install_software.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# update
+
+sudo apt-get update
+sudo apt-get install i2c-tools
+sudo apt-get install python-smbus
+sudo pip install smbus
+
+# install consul
+
+CONSUL_VERSION=1.6.2
+CONSUL_ZIP=/tmp/consul.zip
+wget -O ${CONSUL_ZIP} https://releases.hashicorp.com/consul/${CONSUL_VERSION}/consul_${CONSUL_VERSION}_linux_arm64.zip
+pushd /usr/local/bin
+unzip ${CONSUL_ZIP}
+popd
+
+# install python-consul
+sudo pip install python-consul
+
